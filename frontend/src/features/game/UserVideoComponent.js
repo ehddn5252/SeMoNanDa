0878a6414dee2c17a86bd3f6a57b18d3da5a6e2c
Owen4820 <rkdqudtn712@gmail.com>
2022-08-17 18:38:00 +0900

    아이콘 추가

--- frontend/src/features/game/UserVideoComponent.js
+++ frontend/src/features/game/UserVideoComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import OpenViduVideoComponent from './OvVideo';
-import crown from '../../assets/images/crown.png'
+import crown from '../../assets/images/kingmark.png'
 import './UserVideo.css';
 import replacemnet from '../../assets/images/screen_replacement.png'
-
+import camera from '../../assets/images/camera_on_white.png'
+import mic from '../../assets/images/mic_on_white.png'
 
 export default class UserVideoComponent extends Component {
 
@@ -20,10 +21,14 @@ export default class UserVideoComponent extends Component {
                         <div className="kingstreamcomponent">
                             <OpenViduVideoComponent streamManager={this.props.streamManager} />
                             <div className='crowndiv'><img className='crown' src={crown}></img></div>
+                            <div className='miccontroldiv'><img className='mic' src={mic}></img></div>
+                            <div className='camcontroldiv'><img className='camera' src={camera}></img></div>
                         </div>
                     )
                 : <div className="streamcomponent">
                     <OpenViduVideoComponent streamManager={this.props.streamManager} />
+                    <div className='micdiv'><img className='mic' src={mic}></img></div>
+                    <div className='cameradiv'><img className='camera' src={camera}></img></div>
                 </div>
                 ) : <div className="box">
                         <img className="replacement" src={replacemnet} alt='screen'></img>
